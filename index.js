0a8130579372d1fe6fd67658c69dec6d84b6f13c
Zaid Qtaish <59788372+ZaidQtaish@users.noreply.github.com>
2024-01-03 18:55:12 +0300

    Update index.js
    remove: removed comment

--- index.js
+++ index.js
@@ -159,4 +159,3 @@ app.get('/api/users/:_id/logs', async (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-//    6595797b7d0c359254f1e167
